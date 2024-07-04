61da6d08e677bcbe9c16c6bfce0f392820369dc4
Hardik Chhipa <103168245+FRAGGERR@users.noreply.github.com>
2024-07-04 10:46:35 -0400

    Add files via upload

---
+++ Project_amazon/First.js
@@ -0,0 +1,14 @@
+Full_name = "Hardik Chhipa"; //string value
+age = 21; //number value
+my_laptop_price = 115500.75; //float value
+x = null; //null value
+y = undefined; //undefined value
+completed_js = false; //boolean value
+in_college = true; //boolean value
+console.log(Full_name);
+console.log(age);
+console.log(my_laptop_price);
+console.log(x);
+console.log(completed_js);
+console.log(in_college);
+console.log(typeof (completed_js));
